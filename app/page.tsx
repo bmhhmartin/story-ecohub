f6e721d8cb4a331f78bfc6edb93d479c07becd13
martin-selise <hasnat.martin@selisegroup.com>
2025-10-26 12:26:41 +0600

    boiler plate updated

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores porro animi quis voluptates velit facere tempora soluta laudantium quas. Enim libero nesciunt vel numquam dolores praesentium? Sint neque id animi!
-    </div>
-  );
-}
